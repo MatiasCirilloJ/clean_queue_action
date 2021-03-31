6c0c938c6c62f4a13b1b7ad8dcd7b37c8d79c99f
MatiasCirilloJ <75842869+MatiasCirilloJ@users.noreply.github.com>
2021-03-31 10:53:09 -0300

    Update clean_queue_action.py

--- actions/clean_queue_action.py
+++ actions/clean_queue_action.py
@@ -4,11 +4,5 @@ import os
 from st2common.runners.base_action import Action
 
 class MyEchoAction(Action):
-    def run(self, message):
-        print(message)
-
-        if message == 'working':
-            #os.system('ls')
-            return (True, os.system("st2 run core.remote hosts='10.54.158.195' username='root' private_key='/home/stanley/.ssh/id_rsa' cmd='cd /root ; python rabbitmqa$
-        return (False, message)
-
+    def run(self, queue):
+        return (os.system("st2 run core.remote hosts='10.54.158.195' username='root' private_key='/home/stanley/.ssh/id_rsa' cmd='cd /root ; python rabbitmqadmin purge queue name={}'".format(queue)))
