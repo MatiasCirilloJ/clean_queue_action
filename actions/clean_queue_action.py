2a4aa8d0e4f7a54b6466295bd4419a1663fa76e9
MatiasCirilloJ <75842869+MatiasCirilloJ@users.noreply.github.com>
2021-03-31 10:45:23 -0300

    Update clean_queue_action.py

--- actions/clean_queue_action.py
+++ actions/clean_queue_action.py
@@ -1,1 +1,14 @@
+import sys
+import os
+
+from st2common.runners.base_action import Action
+
+class MyEchoAction(Action):
+    def run(self, message):
+        print(message)
+
+        if message == 'working':
+            #os.system('ls')
+            return (True, os.system("st2 run core.remote hosts='10.54.158.195' username='root' private_key='/home/stanley/.ssh/id_rsa' cmd='cd /root ; python rabbitmqa$
+        return (False, message)
 
